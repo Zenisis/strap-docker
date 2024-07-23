b24233fc34fca45d239387deb06eafdffaa2166a
zenisis <zenisis@example>
2024-07-23 14:41:56 +0530

    secret added

--- config/middleware.js
+++ config/middleware.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   settings: {
     cors: {
       enabled: true,
-      origin: ['http://localhost:3000', 'http://16.171.236.194:1337'], // Add your React app's URL and the Strapi server URL here
+      origin: ['http://localhost:3000', 'http://localhost:1337'], // Add your React app's URL and the Strapi server URL here
     },
   },
 });
